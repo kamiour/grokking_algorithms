d2b4ef143cef83d89f1067f1f1b91bb7951f150b
Mykhailo Kazakov <mykhailo_kazakov@epam.com>
2022-07-04 23:28:09 +0300

    Added quicksort algorithm with a better pivot

--- quicksort.js
+++ quicksort.js
@@ -1,9 +1,24 @@
+// selecting the first element as a pivot
 function sortArray(nums) {
   if (nums.length < 2) {
     return nums;
   }
 
   const pivot = nums.pop();
+  const less = nums.filter((num) => num <= pivot);
+  const greater = nums.filter((num) => num > pivot);
+
+  return [...sortArray(less), pivot, ...sortArray(greater)];
+}
+
+// selecting pivot in the middle of an array
+function sortArray(nums) {
+  if (nums.length < 2) {
+    return nums;
+  }
+
+  const pivotIndex = Math.floor((nums.length - 1) / 2);
+  const pivot = nums.splice(pivotIndex, 1)[0];
   const less = nums.filter((num) => num <= pivot);
   const greater = nums.filter((num) => num > pivot);
 
